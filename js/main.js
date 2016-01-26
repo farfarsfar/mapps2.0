63138cddab3b7b2781e5679e9e65b02265d42637
AxelBylund <wa_gwa@hotmail.com>
2016-01-26 17:31:05 +0100

    added bootstrap and portfolio pictures with extra code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-$('.maps').click(function () {
-    $('.maps iframe').css("pointer-events", "auto");
+// external js: flickity.pkgd.js
+
+$(document).ready(function () {
+	var $gallery = $('.gallery').flickity({
+		cellSelector: 'img',
+		percentPosition: false,
+		lazyload: true,
+		autoPlay: true,
+		wrapAround: true
+	});
+	var $caption = $('.caption');
+	// Flickity instance
+	var flkty = $gallery.data('flickity');
+
+	$gallery.on('cellSelect', function () {
+		// set image caption using img's alt
+		$caption.text(flkty.selectedElement.alt)
+	});
 });
+
+
+$('.maps').click(function () {
+	$('.maps iframe').css("pointer-events", "auto");
+});
+
+console.log("Test")
